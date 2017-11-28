dd56d51871110b54f9204d011b016addeea48575
Enoch2k2 <enoch2k2@hotmail.com>
2017-11-28 10:55:05 -0600

    finished chat-app

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+
+const server = app.listen(3000, function(){
+    console.log('listening on port 3000');
+})
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', function(socket){
+ console.log('made socket connection', socket.id);
+
+ // when server recieves a chat emit, it will call the function with the data
+ socket.on('chat', function(data){
+     // emits the data to all of the clients
+     io.sockets.emit('chat', data);
+ });
+
+ socket.on('typing', function(data){
+     socket.broadcast.emit('typing', data);
+ });
+})
---
+++ public/index.js
@@ -0,0 +1,34 @@
+// Make connection
+var socket = io.connect('http://192.168.0.3:3000');
+
+// inputs
+
+var message = document.getElementById('message');
+var output = document.getElementById('output');
+var btn = document.getElementById('send');
+var handle = document.getElementById('handle');
+var typer = document.getElementById('typer');
+
+// event handlers
+
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+})
+
+message.addEventListener('keydown', function(){
+    socket.emit('typing', handle.value);
+})
+
+// response from server
+
+socket.on('chat', function(data){
+    typer.innerHTML = ""
+    output.innerHTML += "<p><strong>" + data.handle + "</strong>: " + data.message + "</p>";
+})
+
+socket.on('typing', function(data){
+    typer.innerHTML = "<p><em>" + data + " is typing a message...</em></p>"
+})
