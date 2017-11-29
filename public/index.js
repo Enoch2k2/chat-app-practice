2d59b7621f01a1668e825a131a6d625ed7194146
Enoch2k2 <enoch2k2@hotmail.com>
2017-11-28 19:56:16 -0600

    updated chat to be more sleak

--- public/index.js
+++ public/index.js
@@ -8,20 +8,42 @@ var output = document.getElementById('output');
 var btn = document.getElementById('send');
 var handle = document.getElementById('handle');
 var typer = document.getElementById('typer');
-var chat = document.getElementById('Chat-Window');
+var chatWindow = document.getElementById('Chat-Window');
+var chat = document.getElementById('Chat-App');
+var form = document.getElementById('form');
+var messageForm = document.getElementById('message-form');
+var username = document.getElementById('username');
+
+chat.style.display = 'none';
 
 // event handlers
 
-btn.addEventListener('click', function(){
+// btn.addEventListener('click', function(){
+//     socket.emit('chat', {
+//         message: message.value,
+//         handle: username.innerHTML
+//     })
+//     message.value = ''
+// })
+
+messageForm.addEventListener('submit', function(e){
+    e.preventDefault();
     socket.emit('chat', {
         message: message.value,
-        handle: handle.value
+        handle: username.innerHTML
     })
     message.value = ''
 })
 
 message.addEventListener('keydown', function(){
-    socket.emit('typing', handle.value);
+    socket.emit('typing', username.innerHTML);
+})
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    chat.style.display = 'block';
+    username.innerHTML = handle.value;
+    form.style.display = 'none';
 })
 
 // response from server
@@ -29,10 +51,10 @@ message.addEventListener('keydown', function(){
 socket.on('chat', function(data){
     typer.innerHTML = ""
     output.innerHTML += "<p><strong>" + data.handle + "</strong>: " + data.message + "</p>";
-    chat.scrollTop = chat.scrollHeight;
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 })
 
 socket.on('typing', function(data){
     typer.innerHTML = "<p><em>" + data + " is typing a message...</em></p>"
-    chat.scrollTop = chat.scrollHeight;
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 })
